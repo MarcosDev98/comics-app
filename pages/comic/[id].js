d2c123b6ab0d58323d7131d361e39845569305de
Marcos Aguado <marcosolda@outlook.es>
2022-09-04 20:54:10 -0300

    comics scraped again for get specific width and height for each image. Some styles, new dependecy for make a masonry.

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -1,3 +1,45 @@
-export default function Comic() {
-  return <>null</>
+import Head from 'next/head'
+import { Header } from 'components/Header'
+import Image from 'next/image'
+
+
+
+export default function Comic({ id, img, alt, title, width, height }) {
+  return <>
+    <Head>
+        <title>xkcd - Comics for developers</title>
+        <meta name="description" content="Comics for developers" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      
+      <main>
+        <h1>Comic ...</h1>
+        <Image 
+          width={width} 
+          height={height}
+          alt={alt}
+          src={img}
+
+        />
+
+      </main>
+  </>
+}
+
+export async getStaticPaths() {
+  return 
+}
+
+export async function getStaticProps({ params }) {
+
+  const promiseReadFiles = latest
+
+  return {
+    props: {
+
+    }
+  }
+
 }
