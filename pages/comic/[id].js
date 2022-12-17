2b3a2647250a9926f066aa5d43a36638e0cc11da
Marcos Aguado <marcosolda@outlook.es>
2022-12-17 08:17:05 -0300

    comics page, next y previous

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import { Header } from 'components/Header'
 import Image from 'next/image'
+import { readFile, stat } from 'fs/promises'
 
 
 
-export default function Comic({ id, img, alt, title, width, height }) {
+export default function Comic({ img, alt, title, width, height, nextId, prevId, hasNext, hasPrevious }) {
   return <>
     <Head>
         <title>xkcd - Comics for developers</title>
@@ -15,30 +16,59 @@ export default function Comic({ id, img, alt, title, width, height }) {
       <Header />
       
       <main>
-        <h1>Comic ...</h1>
-        <Image 
-          width={width} 
-          height={height}
-          alt={alt}
-          src={img}
-
-        />
-
+        <section className='max-w-lg m-auto'>
+          <h1 className='font-bold'>{title}</h1>
+          <Image 
+            width={width} 
+            height={height} 
+            src={img} 
+            alt={alt}  
+          />
+          
+        </section>
       </main>
   </>
 }
 
-export async getStaticPaths() {
-  return 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: '2500' } }
+    ],
+    fallback: false
+  };
 }
 
 export async function getStaticProps({ params }) {
 
-  const promiseReadFiles = latest
+  const { id } = params
+
+  const content = await readFile(`./comics/${id}.json`, 'utf8')
+  const comic = JSON.parse(content)
+
+  const idNumber = +id
+  const prevId = idNumber - 1
+  const nextId = idNumber + 1
+  
+  // verificar si existe el previo y el siguiente
+  // para navegacion
+  const [prevResult, nextResult] = await Promise.allSettled([
+    stat(`./comics/${prevId}.json`),
+    stat(`./comics/${nextId}.json`)
+  ])
+  
+
+  const hasPrevious = prevResult.status === 'fulfilled'
+  const hasNext = nextResult.status === 'fulfilled'
+  
 
   return {
     props: {
-
+      ...comic,
+      hasPrevious,
+      hasNext,
+      nextId,
+      prevId
     }
   }
 
